ee8522d515494ac24afd449db7e963eee0bef004
yana25-cyber <yana123v@gmail.com>
2025-09-28 19:08:04 -0600

    base de miproyecto

---
+++ testingrepository/src/main/java/com/mycompany/testingrepository/Testingrepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testingrepository;
+
+/**
+ *
+ * @author yana1
+ */
+public class Testingrepository {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
